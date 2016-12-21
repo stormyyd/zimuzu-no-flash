66a13819d8fdd21e6f795f05d1b2a5295128fdf9
Yingdong Yang <storm-yyd@outlook.com>
2016-12-21 13:37:39 +0800

    优化代码

--- zimuzu-no-flash.user.js
+++ zimuzu-no-flash.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name          zimuzu.tv 免 Flash 复制
-// @version       0.2
+// @version       0.3
 // @namespace     stormyyd
 // @description   免 Flash 在 zimuzu.tv 上进行批量复制
 // @license       WTFPL
 // @supportURL    storm-yyd@outlook.com
-// @grant GM_setClipboard
+// @grant         GM_setClipboard
 // @include       http://www.zimuzu.tv/*
 // @include       http://zimuzu.tv/*
 // ==/UserScript==
@@ -19,41 +19,25 @@ $(magnet).unbind();
 $(disk).unbind();
 $(ed2k).siblings("span").remove();
 
+var getUrl = function(urlType){
+    var urls = "";
+    $("div.fl").each(function(){
+        if($(this).children("input").attr("checked") == "checked" && $(this).is(":visible")){
+            url = $(this).siblings("div.fr").children("a[type='" + urlType + "']").attr("href");
+            if(url !== undefined) urls += url + "\n";
+        }
+    });
+    urls = urls.slice(0, -1);
+    GM_setClipboard(urls);
+    alert("下载链接已复制到剪贴板");
+};
+
 $(ed2k).click(function(){
-    $("textarea#UserScript").remove();
-    var urls = "";
-    $("div.fl").each(function(){
-        if($(this).children("input").attr("checked") == "checked" && $(this).is(":visible")){
-            url = $(this).siblings("div.fr").children("a[type='ed2k']").attr("href");
-            urls += url + "\n";
-        }
-    });
-    GM_setClipboard(urls);
-    alert("下载链接已复制到剪贴板");
+    getUrl("ed2k");
 });
-
 $(magnet).click(function(){
-    $("textarea#UserScript").remove();
-    var urls = "";
-    $("div.fl").each(function(){
-        if($(this).children("input").attr("checked") == "checked" && $(this).is(":visible")){
-            url = $(this).siblings("div.fr").children("a[type='magnet']").attr("href");
-            urls += url + "\n";
-        }
-    });
-    GM_setClipboard(urls);
-    alert("下载链接已复制到剪贴板");
+    getUrl("magnet");
 });
-
 $(disk).click(function(){
-    $("textarea#UserScript").remove();
-    var urls = "";
-    $("div.fl").each(function(){
-        if($(this).children("input").attr("checked") == "checked" && $(this).is(":visible")){
-            url = $(this).siblings("div.fr").children("a[type='disk']").attr("href");
-            urls += url + "\n";
-        }
-    });
-    GM_setClipboard(urls);
-    alert("下载链接已复制到剪贴板");
+    getUrl("disk");
 });
